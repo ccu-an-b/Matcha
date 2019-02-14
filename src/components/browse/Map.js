81ba3e9dc4174d1587c5d3bf629ae7f720fa18ee
Trestan <trestan.mervin@gmail.com>
2019-02-14 08:32:13 +0100

    Added another Tile provider: Mapbox

--- src/components/browse/Map.js
+++ src/components/browse/Map.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import { imgPath } from 'helpers';
+
 const defaultLocation = [48.896755, 2.318565];
 const defaultZoom = 13;
 
+const openStreetMapTile = (<TileLayer
+  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+/>);
+
+const mapBoxTile = (<TileLayer
+  url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}"
+  attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+  id='mapbox.streets'
+  accessToken='pk.eyJ1IjoidHJuZGx6IiwiYSI6ImNqczNmd3M2MDI3M3M0NG9hYXZpc2x1MGgifQ.MPY_lgQFNAsJYwdVtmkXrw'
+/>);
+
 export class MapView extends Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
       center: defaultLocation,
@@ -30,14 +43,14 @@ export class MapView extends Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState){
-    const { user, lat, lon ,filtered} = this.props;
+  componentDidUpdate(prevProps, prevState) {
+    const { user, lat, lon, filtered } = this.props;
 
-    if (prevProps !== this.props && lat !== "" && filtered === true){
+    if (prevProps !== this.props && lat !== "" && filtered === true) {
       this.setState({ center: [lat, lon] });
     }
-    else if (prevProps !== this.props && filtered === false){
-      if ( user && user[0].latitude_user && user[0].longitude_user) {
+    else if (prevProps !== this.props && filtered === false) {
+      if (user && user[0].latitude_user && user[0].longitude_user) {
         this.setState({ center: [user[0].latitude_user, user[0].longitude_user] });
       }
       else if (user && user[0].latitude_ip && user[0].longitude_ip) {
@@ -54,7 +67,7 @@ export class MapView extends Component {
   popupContent = (user) => {
     return (
       <div className="map-profile">
-        <img id={user.username} src={imgPath(user.profile_img)} alt="profile_img" className="show-more" onClick={this.props.showMore}/>
+        <img id={user.username} src={imgPath(user.profile_img)} alt="profile_img" className="show-more" onClick={this.props.showMore} />
         <span>
           <h4>{user.username}</h4>
           <h5>{user.age}</h5>
@@ -79,13 +92,10 @@ export class MapView extends Component {
     const { publicUserData } = this.props;
 
     return (
-        <Map center={this.state.center} zoom={this.state.zoom}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          />
-          {publicUserData && this.createMarkersList(publicUserData)}
-        </Map>
+      <Map center={this.state.center} zoom={this.state.zoom}>
+        {mapBoxTile}
+        {publicUserData && this.createMarkersList(publicUserData)}
+      </Map>
     );
   }
 }
