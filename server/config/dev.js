9fcac7a6ea227b8fa6cb69fd7f1cd8cd4267c83c
Chloe CU AN BINH <ccu-an-b@student.42.fr>
2019-03-02 13:00:06 +0100

    auteur + dev.js

---
+++ server/config/dev.js
@@ -0,0 +1,21 @@
+const db = {
+    user: 'ccu-an-b',
+    database: 'basics',
+    password: '',
+    port: '5432'
+};
+
+const db_create = {
+    user: 'ccu-an-b',
+    database: 'postgres',
+    password: '',
+    port: '5432'
+};
+
+ const SECRET = 'djldsdlsjdsd';
+
+ module.exports = {
+    db,
+    db_create ,
+    SECRET
+} 
